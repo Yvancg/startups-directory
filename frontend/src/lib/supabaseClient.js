82c1b3288f5e01b5292641170746304f1a96270c
Yvancg <yvancgh@gmail.com>
2025-08-06 19:51:10 +0700

    updated supabase connx

--- frontend/src/lib/supabaseClient.js
+++ frontend/src/lib/supabaseClient.js
@@ -2,6 +2,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://tziqifersmwisrjxxqrj.supabase.co';
-const supabaseAnonKey = '<eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6aXFpZmVyc213aXNyanh4cXJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYzNDMxNjMsImV4cCI6MjA2MTkxOTE2M30.hZY4dxtxQBjRgY6tefpeyBZrxXqbvBRZC6EPX0inTi4>';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6aXFpZmVyc213aXNyanh4cXJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYzNDMxNjMsImV4cCI6MjA2MTkxOTE2M30.hZY4dxtxQBjRgY6tefpeyBZrxXqbvBRZC6EPX0inTi4';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
